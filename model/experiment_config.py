e70fc415e6c0c6b0750b260127c75db0977de117
Di-Strix <52571870+Di-Strix@users.noreply.github.com>
2026-05-01 18:02:38 +0000

    allow list of values for some config entries

--- model/experiment_config.py
+++ model/experiment_config.py
@@ -7,26 +7,27 @@ from const.model_backend import ModelBackend
 from model.feature_extraction_config import FeatureExtractionConfig
 from model.effecientat_config import EfficientATConfig
 
+type MaybeList[T] = T | list[T]
 
 class _ExperimentBaseConfig(BaseModel):
-    seed: int
+    seed: MaybeList[int] = Field(union_mode='left_to_right')
 
     feature_root: Path
     output_root: Path
 
     feature_extraction: FeatureExtractionConfig
 
-    batch_size: PositiveInt
-    eval_batch_size: PositiveInt
+    batch_size: MaybeList[PositiveInt] = Field(union_mode='left_to_right')
+    eval_batch_size: MaybeList[PositiveInt] = Field(union_mode='left_to_right')
     num_workers: PositiveInt
-    train_crop_seconds: PositiveFloat
+    train_crop_seconds: MaybeList[PositiveFloat] = Field(union_mode='left_to_right')
     epochs: PositiveInt
     early_stopping_min_epoch: PositiveInt
     early_stopping_patience: PositiveInt
 
-    learning_rate: PositiveFloat
-    weight_decay: PositiveFloat
-    dropout: PositiveFloat
+    learning_rate: MaybeList[PositiveFloat] = Field(union_mode='left_to_right')
+    weight_decay: MaybeList[PositiveFloat] = Field(union_mode='left_to_right')
+    dropout: MaybeList[PositiveFloat] = Field(union_mode='left_to_right')
 
     device: str
 
