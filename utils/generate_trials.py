d8012139bd94665053756f1dff3f150fbf65899d
Di-Strix <52571870+Di-Strix@users.noreply.github.com>
2026-05-02 17:02:14 +0000

    fix generate_trials to yield expected result

--- utils/generate_trials.py
+++ utils/generate_trials.py
@@ -8,8 +8,8 @@ from schema.trial_config import TrialConfig
 
 def generate_trials(config: ExperimentConfig) -> Iterator[TrialConfig]:
     dump = config.model_dump()
-    iterable_keys = filter(lambda key: isinstance(dump[key], list), dump.keys())
-    iterable_values = dump.fromkeys(iterable_keys)
+    iterable_keys = list(filter(lambda key: isinstance(dump[key], list), dump.keys()))
+    iterable_values = [dump[key] for key in iterable_keys]
 
     for values in itertools.product(*iterable_values):
         trial_dump = deepcopy(dump)
